afedebc661f15bb90cb1a1178f504198918b79b0
asshurimjoylarita <asshurim@cjnetsolutions.com>
2014-12-10 17:11:55 +0800

    Apply the Logos and make sure the logos are even

--- partials/dt_clients.php
+++ partials/dt_clients.php
@@ -1,9 +1,14 @@
+<?php $logos = get_field('partner_logos', 2); ?>
+
 <div class="container center">
 	<div class="spc-md"></div>
 	<h2 class="condensed">partners</h2>
 	<div class="spc-lg"></div>
 
-	<img src="<?= asset( 'assets/img/clients.png' ) ?>" class="img-responsive" alt=""/>
+		<?php foreach($logos as $logo): ?>
+			<img src="<?=$logo['image']?>" style="height: 60px;" />
+		<?php endforeach; ?>
+<!--	<img src="--><?//= asset( 'assets/img/clients.png' ) ?><!--" class="img-responsive" alt=""/>-->
 
 	<div class="spc-lg"></div>
 </div>
