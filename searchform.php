26411f24e3bc92dc7fc6842ea960d743c0f0e104
asshurimjoylarita <asshurim@cjnetsolutions.com>
2014-12-11 02:48:15 +0800

    Sidebar

--- searchform.php
+++ searchform.php
@@ -1,4 +1,5 @@
 <form method="get" id="searchform" action="<?php bloginfo('url'); ?>/">
+	<h4><i class="fa fa-search"></i> Blog Search...</h4>
 	<div class="input-group">
 		<input type="text" class="form-control" value="<?php the_search_query(); ?>">
                         <span class="input-group-btn">
