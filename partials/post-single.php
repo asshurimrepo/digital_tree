3706f60951bffacc1b3837499eb8865f19a0c5fd
asshurimjoylarita <asshurim@cjnetsolutions.com>
2014-12-10 23:33:30 +0800

    Single Post

--- partials/post-single.php
+++ partials/post-single.php
@@ -4,91 +4,55 @@
 		<div class="col-lg-12">
 
 			<!-- the actual blog post: title/author/date/content -->
-			<h1><a href="">Blog Post Template for Bootstrap</a></h1>
-			<p class="lead"><i class="fa fa-user"></i> by <a href="">Super User</a>
+			<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+
+			<p class="lead"><i class="fa fa-user"></i> by <a href="#"><?php the_author(); ?></a>
 			</p>
 			<hr>
-			<p><i class="fa fa-calendar"></i> Posted on August 24, 2014 at 9:00 PM</p>
-			<p><i class="fa fa-tags"></i> Tags: <a href=""><span class="badge badge-info">Bootstrap</span></a> <a href=""><span class="badge badge-info">Web</span></a> <a href=""><span class="badge badge-info">CSS</span></a> <a href=""><span class="badge badge-info">HTML</span></a></p>
+			<p><i class="fa fa-calendar"></i> Posted on <?php the_time( 'F d, Y' ); ?> at <?php the_time( 'h:m A' ); ?>
+			</p>
+
+			<p><i class="fa fa-tags"></i> Tags: <a href=""><span class="badge badge-info">Bootstrap</span></a> <a
+					href=""><span class="badge badge-info">Web</span></a> <a href=""><span
+						class="badge badge-info">CSS</span></a> <a href=""><span
+						class="badge badge-info">HTML</span></a></p>
 
 			<hr>
-			<img src="http://placehold.it/900x300" class="img-responsive">
+			<?php if ( has_post_thumbnail() ) : ?>
+				<a class="media-left" href="#">
+					<?php the_post_thumbnail( array( 900 ) ); ?>
+				</a>
+			<?php endif; ?>
 			<hr>
-			<p class="lead">Welcome to Bootstrap Blog Template! This is a blog template example, built with Bootstrap. Contains "tools" that would be needed in a post.</p>
-			<p>Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor.</p>
-			<p>Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor. Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor.</p>
-			<p>Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor. Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor.</p>
-			<p>Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor. Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor.</p>
-			<br/>
-			<center><p><strong>Embed Twitter post:</strong>
 
-					<!-- Place this tag in your head or just before your close body tag. -->
-				<blockquote class="twitter-tweet" lang="hu"><p>Thousands of code samples at your fingertips! Literally, thousands: <a href="http://t.co/aHrsBZ7plp">http://t.co/aHrsBZ7plp</a> (via <a href="https://twitter.com/ch9">@ch9</a>) <a href="http://t.co/94CQJLOCzO">pic.twitter.com/94CQJLOCzO</a></p>— Microsoft Developer (@msdev) <a href="https://twitter.com/msdev/statuses/487959572230193152">2014. július 12.</a></blockquote>
-				<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
-			</center>
-			<br/>
-			<center><p><strong>Embed Youtube player:</strong> </center>
+			<?php the_content(); ?>
 
-			<div class="vid">
-				<iframe width="560" height="315" src="//www.youtube.com/embed/bsPUMZlsZP8" frameborder="0" allowfullscreen></iframe>
-			</div>
-			<br/>
-			<p><h3>This Youtube Player is responsive!</h3></p>
-			<p>Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor.</p>
-			<blockquote>
-				<p>„Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.”</p>
-				<footer>Someone famous in <cite title="Source Title">Source Title</cite></footer>
-			</blockquote>
-			<p><h4>I like the post? Like this!</h4></p>
-			<a href="https://twitter.com/share" class="twitter-share-button" data-url="">Tweet</a>
-			<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-
-			<div class="g-plusone" data-annotation="inline" data-width="300" data-href=""></div>
-
-			<!-- Helyezd el ezt a címkét az utolsó +1 gomb címke mögé. -->
-			<script type="text/javascript">
-				(function() {
-					var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-					po.src = 'https://apis.google.com/js/platform.js';
-					var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-				})();
-			</script>
-			<br/>
 			<hr>
 
 			<!-- the comment box -->
 			<div class="well">
 				<h4><i class="fa fa-paper-plane-o"></i> Leave a Comment:</h4>
-				<form role="form">
-					<div class="form-group">
-						<textarea class="form-control" rows="3"></textarea>
-					</div>
-					<button type="submit" class="btn btn-primary"><i class="fa fa-reply"></i> Submit</button>
-				</form>
+
+				<?php
+					if ( comments_open() || get_comments_number() )
+					{
+						comments_template();
+					}
+				?>
 			</div>
-			<script src="https://apis.google.com/js/plusone.js">
-			</script>
+
+
+			<hr >
+
+
+			</div>
+
+
+			</div>
+
+
 			<hr>
 
-			<!-- the comments -->
-			<h3><i class="fa fa-comment"></i> User One says:
-				<small> 9:41 PM on August 24, 2014</small>
-			</h3>
-			<p>Excellent post! Thank You the great article, it was useful!</p>
 
-			<h3><i class="fa fa-comment"></i> User Two says:
-				<small> 9:47 PM on August 24, 2014</small>
-			</h3>
-			<p>Excellent post! Thank You the great article, it was useful!</p>
+			</div>
 
-		</div>
-
-
-	</div>
-
-
-	<hr>
-
-
-
-</div>
