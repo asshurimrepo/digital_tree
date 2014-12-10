99d949ea84771d7ff06dc95b89f67a50ab77c580
asshurimjoylarita <asshurim@cjnetsolutions.com>
2014-12-10 21:08:20 +0800

    add_theme_support( 'post-thumbnails' );

--- functions.php
+++ functions.php
@@ -1,8 +1,9 @@
 <?php
 
-require_once('inc/helpers.php');
-require_once('inc/hooks.php');
+	require_once( 'inc/helpers.php' );
+	require_once( 'inc/hooks.php' );
 
-add_theme_support('menus');
+	add_theme_support( 'menus' );
+	add_theme_support( 'post-thumbnails' );
 
 
