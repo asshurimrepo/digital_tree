e672673eaa6dbb54ff000b4caaafdad24ab808be
asshurimjoylarita <asshurim@cjnetsolutions.com>
2014-12-11 01:34:37 +0800

    Partner Logos Margin

--- blog-list.php
+++ blog-list.php
@@ -5,7 +5,7 @@
 <!--		<a href="--><?php //the_permalink() ?><!--"><img src="--><?//= asset( 'assets/img/profile-spc.png' ) ?><!--"/></a>-->
 
 		<div id="box-6" class="box">
-			<?php the_post_thumbnail('post-thumbnail'); ?>
+			<?php the_post_thumbnail('small'); ?>
 <!--			<img id="image-6" src="--><?//=$featured_image_url?><!--"/>-->
 			<span class="caption scale-caption">
 				<p><?php the_excerpt(); ?></p>
