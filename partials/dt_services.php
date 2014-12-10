fe969806af7d2503efb45dc9fb20c35d0a52ed3b
asshurimjoylarita <asshurim@cjnetsolutions.com>
2014-12-10 14:09:54 +0800

    ACF for Services

--- partials/dt_services.php
+++ partials/dt_services.php
@@ -1,9 +1,11 @@
+<?php $services = get_field('services', 2); var_dump($services); ?>
+
 <section id="services" class="dark">
 	<div class="container">
 
 		<h1 class="center head-title white">
 			our services
-			<small>lorem ipsum dolor sit amet</small>
+<!--			<small>lorem ipsum dolor sit amet</small>-->
 
 			<img class="img-responsive center shadow" src="<?= asset( 'assets/img/shadow.png' ) ?>"/>
 		</h1>
@@ -15,14 +17,11 @@
 
 				<h3 class="fw800 center"><i class="fa fa-edit block"></i>
 					<div class="spc-md"></div>
-					Graphic Designs
+					Social Media
 				</h3>
 
 				<div class="spc-md"></div>
-
-				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
-					laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
-					ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+				<p>The term “Social Media” has been changing and is associated with different terminologies. For one to spot quickly the “things” mainly connected or “associated” with social media, one may think of “electronically connecting with people”. Social media is fast growing and has altered every communication man has ever created. To name a broad example of it is electronic mail and e-marketing.</p>
 			</div>
 
 			<div class="col-md-4">
