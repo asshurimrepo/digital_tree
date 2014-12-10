0c0c99ed60e82b6162a4b6739a7166901fe5d6e2
asshurimjoylarita <asshurim@cjnetsolutions.com>
2014-12-10 14:15:20 +0800

    ACF for Services

--- partials/dt_services.php
+++ partials/dt_services.php
@@ -1,4 +1,4 @@
-<?php $services = get_field('services', 2); var_dump($services); ?>
+<?php $services = get_field('services', 2); /*var_dump($services)*/; ?>
 
 <section id="services" class="dark">
 	<div class="container">
@@ -13,83 +13,31 @@
 
 		<div class="row">
 
+
+			<?php foreach($services as $i=>$service): ?>
+
 			<div class="col-md-4">
 
-				<h3 class="fw800 center"><i class="fa fa-edit block"></i>
+				<h3 class="fw800 center"><?=$service->icon?>
 					<div class="spc-md"></div>
-					Social Media
+					<?=$service->title?>
 				</h3>
 
 				<div class="spc-md"></div>
-				<p>The term “Social Media” has been changing and is associated with different terminologies. For one to spot quickly the “things” mainly connected or “associated” with social media, one may think of “electronically connecting with people”. Social media is fast growing and has altered every communication man has ever created. To name a broad example of it is electronic mail and e-marketing.</p>
+				<p><?=$service->description?></p>
 			</div>
 
-			<div class="col-md-4">
-				<h3 class="fw800 center"><i class="fa fa-html5 block"></i>
-					<div class="spc-md"></div>
-					Web Development
-				</h3>
+				<?php if($i%3 == 2): ?> <div class="col-md-12 spc-md"></div> <?php endif; ?>
 
-				<div class="spc-md"></div>
+			<?php endforeach; ?>
 
-				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
-					laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
-					ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
-			</div>
+<!--			-->
 
-			<div class="col-md-4">
-				<h3 class="fw800 center"><i class="fa fa-shopping-cart block"></i>
-					<div class="spc-md"></div>
-					E-Commerce
-				</h3>
 
-				<div class="spc-md"></div>
 
-				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
-					laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
-					ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
-			</div>
 
-			<div class="col-md-12 spc-md"></div>
 
-			<div class="col-md-4">
-				<h3 class="fw800 center"><i class="fa fa-edit block"></i>
-					<div class="spc-md"></div>
-					Graphic Designs
-				</h3>
 
-				<div class="spc-md"></div>
-
-				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
-					laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
-					ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
-			</div>
-
-			<div class="col-md-4">
-				<h3 class="fw800 center"><i class="fa fa-edit block"></i>
-					<div class="spc-md"></div>
-					Graphic Designs
-				</h3>
-
-				<div class="spc-md"></div>
-
-				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
-					laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
-					ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
-			</div>
-
-			<div class="col-md-4">
-				<h3 class="fw800 center"><i class="fa fa-edit block"></i>
-					<div class="spc-md"></div>
-					Graphic Designs
-				</h3>
-
-				<div class="spc-md"></div>
-
-				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
-					laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
-					ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
-			</div>
 
 		</div>
 
