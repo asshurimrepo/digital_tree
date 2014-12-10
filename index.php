db905e8509bdc67d0480340383d350185e3c411b
asshurimjoylarita <asshurim@cjnetsolutions.com>
2014-12-10 21:31:24 +0800

    Blog Posts

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 		get_template_part( 'partials/dt_highlights' );
 		get_template_part( 'partials/dt_clients' );
 		get_template_part( 'partials/dt_services' );
-		get_template_part( 'partials/dt_team' );
+		get_template_part( 'partials/dt_blogs' );
 		get_template_part( 'partials/dt_hiring' );
 		get_template_part( 'partials/dt_contact_us' );
 
---
+++ partials/dt_blogs.php
@@ -0,0 +1,61 @@
+<?php
+	query_posts();
+?>
+
+	<div id="team" class="mid-dark">
+
+		<div class="spc-md"></div>
+
+		<div class="container">
+
+			<h1 class="center head-title">
+				blogs
+				<!--			<small>lorem ipsum dolor sit amet</small>-->
+
+				<img class="img-responsive center shadow" src="<?= asset( 'assets/img/shadow.png' ) ?>"/>
+			</h1>
+
+
+			<div class="row">
+
+				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+					<?php $featured_image_url = wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) ); ?>
+
+					<div class="col-md-4">
+
+						<div class="profile" style="background-image: url(<?= $featured_image_url ?>);">
+							<img src="<?= asset( 'assets/img/profile-spc.png' ) ?>"/>
+						</div>
+
+						<div class="profile-info">
+							<h3 style="margin: 5px;"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+
+								<small class="fw300"><br/> <i><?php the_time( 'M d Y' ); ?></i></small>
+
+								<small class="pull-right">
+									<a href="#"><i class="fa fa-facebook-square"></i></a>
+									<a href="#"><i class="fa fa-twitter-square"></i></a>
+									<a href="#"><i class="fa fa-pinterest-square"></i></a>
+								</small>
+							</h3>
+						</div>
+
+					</div>
+
+					<!--			<div class="spc-md col-md-12"></div>-->
+
+				<?php endwhile; endif; ?>
+
+			</div>
+
+		</div>
+
+		<div class="spc-lg"></div>
+
+	</div>
+
+	<div class="shadow2"></div>
+
+
+<?php wp_reset_query();
