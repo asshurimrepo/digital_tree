2fc129111e98c78b0d9a3668008b8b9761ee7c30
asshurimjoylarita <asshurim@cjnetsolutions.com>
2014-12-11 02:33:07 +0800

    BLogs

--- functions.php
+++ functions.php
@@ -6,4 +6,14 @@
 	add_theme_support( 'menus' );
 	add_theme_support( 'post-thumbnails' );
 
+	register_sidebar( array(
+		'name'          => __( 'Primary Sidebar', 'digitaltree' ),
+		'id'            => 'sidebar-1',
+		'description'   => __( 'Main sidebar that appears on the left.', 'digitaltree' ),
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h1 class="widget-title">',
+		'after_title'   => '</h1>',
+	) );
+
 
