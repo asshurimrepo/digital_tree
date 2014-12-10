32e7ac13ef0fa0a44597e564b8f859d81d943a4b
asshurimjoylarita <asshurim@cjnetsolutions.com>
2014-12-10 21:39:55 +0800

    Blog Posts

---
+++ blog-list.php
@@ -0,0 +1,20 @@
+<div class="col-md-4">
+
+	<div class="profile" style="background-image: url(<?= $featured_image_url ?>);">
+		<a href="<?php the_permalink() ?>"><img src="<?= asset( 'assets/img/profile-spc.png' ) ?>"/></a>
+	</div>
+
+	<div class="profile-info">
+		<h3 style="margin: 5px;"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+
+			<small class="fw300"><br/> <i><?php the_time( 'M d Y' ); ?></i></small>
+
+			<small class="pull-right">
+				<a href="#"><i class="fa fa-facebook-square"></i></a>
+				<a href="#"><i class="fa fa-twitter-square"></i></a>
+				<a href="#"><i class="fa fa-pinterest-square"></i></a>
+			</small>
+		</h3>
+	</div>
+
+</div>
