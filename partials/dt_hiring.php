8057b0f5ca231da8b6c254f4ea9dc3259d5047e1
asshurimjoylarita <asshurim@cjnetsolutions.com>
2014-12-10 22:58:00 +0800

    mapadako pa nimo ang tango logo?

--- partials/dt_hiring.php
+++ partials/dt_hiring.php
@@ -4,7 +4,7 @@
 
 		<h1 class="center head-title white">
 			now hiring
-			<small>lorem ipsum dolor sit amet</small>
+<!--			<small>lorem ipsum dolor sit amet</small>-->
 
 			<img class="img-responsive center shadow" src="<?= asset( 'assets/img/shadow.png' ) ?>"/>
 		</h1>
@@ -15,9 +15,9 @@
 			<div class="col-md-10 col-md-offset-1">
 				<div class="featured-box">
 
-					<h3 class="condensed">Web Developer</h3>
-					<div class="spc-md"></div>
-					<p class="fw300">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.  Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
+<!--					<h3 class="condensed">Web Developer</h3>-->
+<!--					<div class="spc-md"></div>-->
+					<p class="fw300">The Digital Tree seeks inside/outside web developers, designers, marketing directors, and salesperson. Excellent salary and commission. PREVIOUS EXPERIENCE NECESSARY. For submission of application, refer below.</p>
 
 				</div>
 			</div>
