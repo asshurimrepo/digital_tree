f74fc876c7cc7daaa1cbad8eabfc3bf686e320b4
asshurimjoylarita <asshurim@cjnetsolutions.com>
2014-12-12 14:49:55 +0800

    Partners in Mobile

--- partials/dt_clients.php
+++ partials/dt_clients.php
@@ -6,9 +6,10 @@
 	<div class="spc-lg"></div>
 
 		<?php foreach($logos as $i=>$logo): ?>
-			<img src="<?=$logo['image']?>" style="height: <?=$logo['size']?>px; padding: 0 15px; margin-bottom: 15px;" />
+			<img src="<?=$logo['image']?>" class="hidden-xs" style="height: <?=$logo['size']?>px; padding: 0 15px; margin-bottom: 15px;" />
+			<img src="<?=$logo['image']?>" class="img-responsive visible-xs" alt="Digital Tree Partners" style="max-height: 50px;" />
+
 		<?php endforeach; ?>
-<!--	<img src="--><?//= asset( 'assets/img/clients.png' ) ?><!--" class="img-responsive" alt=""/>-->
 
 	<div class="spc-lg"></div>
 </div>
