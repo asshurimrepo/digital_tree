fc08e03af5cb91ec4946284160fc53743db4f23b
asshurimjoylarita <asshurim@cjnetsolutions.com>
2014-12-16 01:55:46 +0800

    RSS Feeds

--- partials/blog-rss.php
+++ partials/blog-rss.php
@@ -8,7 +8,7 @@
 ?>
 <div class="col-md-4">
 
-	<div class="profile" style="background-image: url(<?= $featured_image_url[0]?>);">
+	<div class="profile" style="background-image: url('<?= $featured_image_url[0]?>');">
 
 		<div id="box-6" class="box">
 			<a href="<?=$rss_item['link']?>"><img src="<?= asset( 'assets/img/profile-spc.png' ) ?>"/></a>
