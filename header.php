538b88d90796fa14fecadfe2e95c0fe711434b5e
asshurimjoylarita <asshurim@cjnetsolutions.com>
2014-12-18 20:47:50 +0800

    Updates

--- header.php
+++ header.php
@@ -16,6 +16,8 @@
     <title><?php bloginfo('name'); ?>  <?php wp_title(); ?></title>
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
+    <link rel="shortcut icon" href="<?=asset('img/favicon.ico')?>">
+
 <!-- Jquery -->
     <?=link_js('assets/jquery/jquery.min.js')?>
 
