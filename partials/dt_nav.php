477d75904312f366083fd2dcc4c0d1a7ea6cf945
asshurimjoylarita <asshurim@cjnetsolutions.com>
2015-01-13 15:05:36 +0800

    Updates

--- partials/dt_nav.php
+++ partials/dt_nav.php
@@ -1,39 +1,43 @@
 <?php global $main_nav; ?>
 
-<nav class="navbar navbar-default  main-navbar" role="navigation" id="home">
-    <div class="container">
+<header>
 
-        <div class="row">
-            <div class="col-md-12 ">
-                <figure class="logo">
-                    <img src="<?=asset('assets/img/logo.svg')?>" class="img-responsive" alt="Alfareeda Logo"/>
-                </figure>
-            </div>
-        </div>
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                <span class="text-center">Menu</span>
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-        </div>
+	<nav class="navbar navbar-default  main-navbar" role="navigation" id="home">
+		<div class="container">
 
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse " id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav onepager navbar-right">
+			<div class="row">
+				<div class="col-md-12 ">
+					<figure class="logo">
+						<img src="<?=asset('assets/img/logo.svg')?>" class="img-responsive" alt="Alfareeda Logo"/>
+					</figure>
+				</div>
+			</div>
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					<span class="text-center">Menu</span>
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+			</div>
 
-                <?php foreach($main_nav as $nav): ?>
-                    <li class="<?=is_active($nav->object_id)?>">
-                        <a href="<?=$nav->url?>"><?=$nav->title?></a>
-                    </li>
-                <?php endforeach; ?>
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse " id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav onepager navbar-right">
 
-            </ul>
+					<?php foreach($main_nav as $nav): ?>
+						<li class="<?=is_active($nav->object_id)?>">
+							<a href="<?=$nav->url?>"><?=$nav->title?></a>
+						</li>
+					<?php endforeach; ?>
+
+				</ul>
 
 
-        </div><!-- /.navbar-collapse -->
-    </div>
-</nav>
+			</div><!-- /.navbar-collapse -->
+		</div>
+	</nav>
+
+</header>
