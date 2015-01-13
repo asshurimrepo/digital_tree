91b245a3e98f744ba01e329fe85bd3144a5b1cd7
asshurimjoylarita <asshurim@cjnetsolutions.com>
2015-01-13 21:56:26 +0800

    Updates

--- partials/dt_digi_tree.php
+++ partials/dt_digi_tree.php
@@ -85,8 +85,10 @@
 	</foreignObject>
 
 
-	<foreignObject class="node video" x="0" y="0" width="100%" height="100%">
-		<img src="<?=asset('assets/img/img2.jpg')?>" class="img2 hide" />
+	<foreignObject class="node spot1" x="0" y="0" width="100%" height="100%">
+			<div>
+				<img src="<?=asset('assets/img/img2.jpg')?>" class="img2 hide" />
+			</div>
 	</foreignObject>
 
 </svg>
