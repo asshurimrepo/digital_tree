cbf5c2ebc78657c7c447c2ba41db3da5ba0b536d
asshurimjoylarita <asshurim@cjnetsolutions.com>
2015-01-14 17:18:13 +0800

    Updates

--- digi-tree/digi-tree_edge.js
+++ digi-tree/digi-tree_edge.js
@@ -1,7 +1,109 @@
+(function (compId) {
+    var site_url = $('meta[name=site_url]').prop('content');
 
-(function(compId){var _=null,y=true,n=false,zy='scaleY',x18='rgba(255,255,255,0.00)',x5='rgba(0,0,0,0)',e30='${Pasted5}',zx='scaleX',x4='both',e22='${Pasted4}',e23='${root2}',x3='5.0.1.386',lf='left',e26='${Pasted112}',h='height',e29='${Pasted10}',x1='5.0.1',e27='${Pasted6}',x2='5.0.0',e25='${Pasted7}',w='width',tp='top',e28='${Pasted9}',g='image',e24='${Pasted2}',e19='${_1}',e21='${Pasted13}',e20='${Pasted8}';var g15='Pasted9.svg',g14='Pasted8.svg',g8='Pasted2.svg',g11='Pasted5.svg',g16='Pasted10.svg',g17='Pasted13.svg',g6='root2.svg',g7='1.svg',g10='Pasted4.svg',g9='Pasted11.svg',g12='Pasted6.svg',g13='Pasted7.svg';var im='images/',aud='media/',vid='media/',js='js/',fonts={},opts={'gAudioPreloadPreference':'auto','gVideoPreloadPreference':'auto'},resources=[],scripts=[],symbols={"stage":{v:x1,mv:x2,b:x3,stf:x4,cg:x4,rI:n,cn:{dom:[{id:'root2',t:g,r:['354px','468px','4767px','4767px','auto','auto'],autoOrient:true,f:[x5,im+g6,'0px','0px'],tf:[[],[],[],['0.03','0.03']]},{id:'_1',t:g,r:['187px','611px','157px','157px','auto','auto'],f:[x5,im+g7,'0px','0px'],tf:[[],[],[],['0','0']]},{id:'Pasted2',t:g,r:['367px','301px','157px','157px','auto','auto'],f:[x5,im+g8,'0px','0px']},{id:'Pasted112',t:g,r:['589px','223px','158px','157px','auto','auto'],f:[x5,im+g9,'0px','0px','100%','100%','no-repeat'],tf:[[],[],[],['1.56','1.56']]},{id:'Pasted4',t:g,r:['456px','213px','69px','69px','auto','auto'],f:[x5,im+g10,'0px','0px']},{id:'Pasted5',t:g,r:['632px','92px','69px','69px','auto','auto'],f:[x5,im+g11,'0px','0px']},{id:'Pasted6',t:g,r:['365px','38px','68px','68px','auto','auto'],f:[x5,im+g12,'0px','0px']},{id:'Pasted7',t:g,r:['277px','37px','69px','69px','auto','auto'],f:[x5,im+g13,'0px','0px']},{id:'Pasted8',t:g,r:['278px','123px','158px','158px','auto','auto'],f:[x5,im+g14,'0px','0px']},{id:'Pasted9',t:g,r:['455px','3px','158px','157px','auto','auto'],f:[x5,im+g15,'0px','0px']},{id:'Pasted10',t:g,r:['10px','37px','245px','246px','auto','auto'],f:[x5,im+g16,'0px','0px']},{id:'Pasted13',t:g,r:['99px','301px','68px','68px','auto','auto'],f:[x5,im+g17,'0px','0px']}],style:{'${Stage}':{isStage:true,r:['null','null','798px','611px','auto','auto'],overflow:'hidden',f:[x18]}}},tt:{d:4095,a:y,data:[["eid6",zx,0,1000,"easeOutElastic",e19,'0','1'],["eid66",zy,1908,1000,"easeOutElastic",e20,'0','1'],["eid80",zx,2500,1595,"easeOutElastic",e21,'0','1'],["eid53",lf,2095,0,"easeOutElastic",e22,'456px','456px'],["eid51",zy,1095,1000,"easeOutElastic",e22,'0','1'],["eid50",zx,1095,1000,"easeOutElastic",e22,'0','1'],["eid17",w,0,482,"easeOutCubic",e23,'85px','4767px'],["eid10",zy,394,1000,"easeOutElastic",e24,'0','1'],["eid62",tp,1838,912,"easeOutElastic",e25,'347px','37px'],["eid20",zx,0,0,"easeOutElastic",e23,'0.03','0.03'],["eid8",tp,88,912,"easeOutElastic",e19,'611px','301px'],["eid34",zy,750,1000,"easeOutElastic",e26,'0','1.56'],["eid67",tp,1996,912,"easeOutElastic",e20,'432px','123px'],["eid9",tp,482,912,"easeOutElastic",e24,'611px','301px'],["eid33",tp,838,912,"easeOutElastic",e26,'488px','223px'],["eid64",lf,2750,0,"easeOutElastic",e25,'277px','277px'],["eid59",tp,1588,912,"easeOutElastic",e27,'348px','38px'],["eid71",zy,2095,1000,"easeOutElastic",e28,'0','1'],["eid73",zx,2363,1000,"easeOutElastic",e29,'0','1'],["eid81",zy,2500,1595,"easeOutElastic",e21,'0','1'],["eid82",tp,2640,1455,"easeOutElastic",e21,'611px','301px'],["eid74",zy,2363,1000,"easeOutElastic",e29,'0','1'],["eid54",zx,1250,1000,"easeOutElastic",e30,'0','1'],["eid7",zy,0,1000,"easeOutElastic",e19,'0','1'],["eid57",zx,1500,1000,"easeOutElastic",e27,'0','1'],["eid60",zx,1750,1000,"easeOutElastic",e25,'0','1'],["eid36",zx,750,1000,"easeOutElastic",e26,'0','1.56'],["eid70",zx,2095,1000,"easeOutElastic",e28,'0','1'],["eid69",lf,2908,0,"easeOutElastic",e20,'278px','278px'],["eid21",zy,0,0,"easeOutElastic",e23,'0.03','0.03'],["eid72",tp,2183,912,"easeOutElastic",e28,'313px','3px'],["eid16",h,0,482,"easeOutCubic",e23,'137px','4767px'],["eid23","location",0,0,"easeOutQuad",e23,[[398.33,539.28,0,0,0,0,0],[398.33,539.28,0,0,0,0,0]]],["eid46",lf,1750,0,"easeOutElastic",e26,'589px','589px'],["eid58",zy,1500,1000,"easeOutElastic",e27,'0','1'],["eid52",tp,1183,912,"easeOutElastic",e22,'521px','213px'],["eid55",zy,1250,1000,"easeOutElastic",e30,'0','1'],["eid77",lf,4095,0,"easeOutElastic",e29,'10px','10px'],["eid61",zy,1750,1000,"easeOutElastic",e25,'0','1'],["eid75",tp,2451,912,"easeOutElastic",e29,'347px','37px'],["eid65",zx,1908,1000,"easeOutElastic",e20,'0','1'],["eid56",tp,1338,912,"easeOutElastic",e30,'402px','92px'],["eid12",zx,394,1000,"easeOutElastic",e24,'0','1']]}}};AdobeEdge.registerCompositionDefn(compId,symbols,fonts,scripts,resources,opts);})("EDGE-187302");
-(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;Edge.registerEventBinding(compId,function($){
+    var _ = null, y = true, n = false, zy = 'scaleY', x18 = 'rgba(255,255,255,0.00)', x5 = 'rgba(0,0,0,0)', e30 = '${Pasted5}', zx = 'scaleX', x4 = 'both', e22 = '${Pasted4}', e23 = '${root2}', x3 = '5.0.1.386', lf = 'left', e26 = '${Pasted112}', h = 'height', e29 = '${Pasted10}', x1 = '5.0.1', e27 = '${Pasted6}', x2 = '5.0.0', e25 = '${Pasted7}', w = 'width', tp = 'top', e28 = '${Pasted9}', g = 'image', e24 = '${Pasted2}', e19 = '${_1}', e21 = '${Pasted13}', e20 = '${Pasted8}';
+    var g15 = 'Pasted9.svg', g14 = 'Pasted8.svg', g8 = 'Pasted2.svg', g11 = 'Pasted5.svg', g16 = 'Pasted10.svg', g17 = 'Pasted13.svg', g6 = 'root2.svg', g7 = '1.svg', g10 = 'Pasted4.svg', g9 = 'Pasted11.svg', g12 = 'Pasted6.svg', g13 = 'Pasted7.svg';
+    var im = site_url+'/images/', aud = 'media/', vid = 'media/', js = 'js/', fonts = {}, opts = {
+        'gAudioPreloadPreference': 'auto',
+        'gVideoPreloadPreference': 'auto'
+    }, resources = [], scripts = [], symbols = {
+        "stage": {
+            v: x1,
+            mv: x2,
+            b: x3,
+            stf: x4,
+            cg: x4,
+            rI: n,
+            cn: {
+                dom: [{
+                    id: 'root2',
+                    t: g,
+                    r: ['354px', '468px', '4767px', '4767px', 'auto', 'auto'],
+                    autoOrient: true,
+                    f: [x5, im + g6, '0px', '0px'],
+                    tf: [[], [], [], ['0.03', '0.03']]
+                }, {
+                    id: '_1',
+                    t: g,
+                    r: ['187px', '611px', '157px', '157px', 'auto', 'auto'],
+                    f: [x5, im + g7, '0px', '0px'],
+                    tf: [[], [], [], ['0', '0']]
+                }, {
+                    id: 'Pasted2',
+                    t: g,
+                    r: ['367px', '301px', '157px', '157px', 'auto', 'auto'],
+                    f: [x5, im + g8, '0px', '0px']
+                }, {
+                    id: 'Pasted112',
+                    t: g,
+                    r: ['589px', '223px', '158px', '157px', 'auto', 'auto'],
+                    f: [x5, im + g9, '0px', '0px', '100%', '100%', 'no-repeat'],
+                    tf: [[], [], [], ['1.56', '1.56']]
+                }, {
+                    id: 'Pasted4',
+                    t: g,
+                    r: ['456px', '213px', '69px', '69px', 'auto', 'auto'],
+                    f: [x5, im + g10, '0px', '0px']
+                }, {
+                    id: 'Pasted5',
+                    t: g,
+                    r: ['632px', '92px', '69px', '69px', 'auto', 'auto'],
+                    f: [x5, im + g11, '0px', '0px']
+                }, {
+                    id: 'Pasted6',
+                    t: g,
+                    r: ['365px', '38px', '68px', '68px', 'auto', 'auto'],
+                    f: [x5, im + g12, '0px', '0px']
+                }, {
+                    id: 'Pasted7',
+                    t: g,
+                    r: ['277px', '37px', '69px', '69px', 'auto', 'auto'],
+                    f: [x5, im + g13, '0px', '0px']
+                }, {
+                    id: 'Pasted8',
+                    t: g,
+                    r: ['278px', '123px', '158px', '158px', 'auto', 'auto'],
+                    f: [x5, im + g14, '0px', '0px']
+                }, {
+                    id: 'Pasted9',
+                    t: g,
+                    r: ['455px', '3px', '158px', '157px', 'auto', 'auto'],
+                    f: [x5, im + g15, '0px', '0px']
+                }, {
+                    id: 'Pasted10',
+                    t: g,
+                    r: ['10px', '37px', '245px', '246px', 'auto', 'auto'],
+                    f: [x5, im + g16, '0px', '0px']
+                }, {
+                    id: 'Pasted13',
+                    t: g,
+                    r: ['99px', '301px', '68px', '68px', 'auto', 'auto'],
+                    f: [x5, im + g17, '0px', '0px']
+                }],
+                style: {
+                    '${Stage}': {
+                        isStage: true,
+                        r: ['null', 'null', '798px', '611px', 'auto', 'auto'],
+                        overflow: 'hidden',
+                        f: [x18]
+                    }
+                }
+            },
+            tt: {
+                d: 4095,
+                a: y,
+                data: [["eid6", zx, 0, 1000, "easeOutElastic", e19, '0', '1'], ["eid66", zy, 1908, 1000, "easeOutElastic", e20, '0', '1'], ["eid80", zx, 2500, 1595, "easeOutElastic", e21, '0', '1'], ["eid53", lf, 2095, 0, "easeOutElastic", e22, '456px', '456px'], ["eid51", zy, 1095, 1000, "easeOutElastic", e22, '0', '1'], ["eid50", zx, 1095, 1000, "easeOutElastic", e22, '0', '1'], ["eid17", w, 0, 482, "easeOutCubic", e23, '85px', '4767px'], ["eid10", zy, 394, 1000, "easeOutElastic", e24, '0', '1'], ["eid62", tp, 1838, 912, "easeOutElastic", e25, '347px', '37px'], ["eid20", zx, 0, 0, "easeOutElastic", e23, '0.03', '0.03'], ["eid8", tp, 88, 912, "easeOutElastic", e19, '611px', '301px'], ["eid34", zy, 750, 1000, "easeOutElastic", e26, '0', '1.56'], ["eid67", tp, 1996, 912, "easeOutElastic", e20, '432px', '123px'], ["eid9", tp, 482, 912, "easeOutElastic", e24, '611px', '301px'], ["eid33", tp, 838, 912, "easeOutElastic", e26, '488px', '223px'], ["eid64", lf, 2750, 0, "easeOutElastic", e25, '277px', '277px'], ["eid59", tp, 1588, 912, "easeOutElastic", e27, '348px', '38px'], ["eid71", zy, 2095, 1000, "easeOutElastic", e28, '0', '1'], ["eid73", zx, 2363, 1000, "easeOutElastic", e29, '0', '1'], ["eid81", zy, 2500, 1595, "easeOutElastic", e21, '0', '1'], ["eid82", tp, 2640, 1455, "easeOutElastic", e21, '611px', '301px'], ["eid74", zy, 2363, 1000, "easeOutElastic", e29, '0', '1'], ["eid54", zx, 1250, 1000, "easeOutElastic", e30, '0', '1'], ["eid7", zy, 0, 1000, "easeOutElastic", e19, '0', '1'], ["eid57", zx, 1500, 1000, "easeOutElastic", e27, '0', '1'], ["eid60", zx, 1750, 1000, "easeOutElastic", e25, '0', '1'], ["eid36", zx, 750, 1000, "easeOutElastic", e26, '0', '1.56'], ["eid70", zx, 2095, 1000, "easeOutElastic", e28, '0', '1'], ["eid69", lf, 2908, 0, "easeOutElastic", e20, '278px', '278px'], ["eid21", zy, 0, 0, "easeOutElastic", e23, '0.03', '0.03'], ["eid72", tp, 2183, 912, "easeOutElastic", e28, '313px', '3px'], ["eid16", h, 0, 482, "easeOutCubic", e23, '137px', '4767px'], ["eid23", "location", 0, 0, "easeOutQuad", e23, [[398.33, 539.28, 0, 0, 0, 0, 0], [398.33, 539.28, 0, 0, 0, 0, 0]]], ["eid46", lf, 1750, 0, "easeOutElastic", e26, '589px', '589px'], ["eid58", zy, 1500, 1000, "easeOutElastic", e27, '0', '1'], ["eid52", tp, 1183, 912, "easeOutElastic", e22, '521px', '213px'], ["eid55", zy, 1250, 1000, "easeOutElastic", e30, '0', '1'], ["eid77", lf, 4095, 0, "easeOutElastic", e29, '10px', '10px'], ["eid61", zy, 1750, 1000, "easeOutElastic", e25, '0', '1'], ["eid75", tp, 2451, 912, "easeOutElastic", e29, '347px', '37px'], ["eid65", zx, 1908, 1000, "easeOutElastic", e20, '0', '1'], ["eid56", tp, 1338, 912, "easeOutElastic", e30, '402px', '92px'], ["eid12", zx, 394, 1000, "easeOutElastic", e24, '0', '1']]
+            }
+        }
+    };
+    AdobeEdge.registerCompositionDefn(compId, symbols, fonts, scripts, resources, opts);
+})("EDGE-187302");
+(function ($, Edge, compId) {
+    var Composition = Edge.Composition, Symbol = Edge.Symbol;
+    Edge.registerEventBinding(compId, function ($) {
 //Edge symbol: 'stage'
-(function(symbolName){})("stage");
+        (function (symbolName) {
+        })("stage");
 //Edge symbol end:'stage'
-})})(AdobeEdge.$,AdobeEdge,"EDGE-187302");
+    })
+})(AdobeEdge.$, AdobeEdge, "EDGE-187302");
