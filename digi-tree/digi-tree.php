252995269123b33b5309cb18c34464bbde0bd8f2
asshurimjoylarita <asshurim@cjnetsolutions.com>
2015-01-14 17:09:53 +0800

    Updates

--- digi-tree/digi-tree.php
+++ digi-tree/digi-tree.php
@@ -1,7 +1,6 @@
 
 <!--Adobe Edge Runtime-->
-<meta http-equiv="X-UA-Compatible" content="IE=Edge">
-<script type="text/javascript" charset="utf-8" src="http://animate.adobe.com/runtime/5.0.1/edge.5.0.1.min.js"></script>
+<script type="text/javascript" charset="utf-8" src="edge_includes/edge.5.0.1.min.js"></script>
 <style>
 .edgeLoad-EDGE-187302 { visibility:hidden; }
 </style>
@@ -19,18 +18,18 @@
 <!--Adobe Edge Runtime End-->
 
 
-	<div id="Stage" class="EDGE-187302">
-	
-        <div id="Stage_root2" class="edgeLoad-EDGE-187302">huhuhu</div>
-        <div id="Stage__1" class="edgeLoad-EDGE-187302"></div>
-        <div id="Stage_Pasted2" class="edgeLoad-EDGE-187302"></div>
-        <div id="Stage_Pasted112" class="edgeLoad-EDGE-187302"></div>
-        <div id="Stage_Pasted4" class="edgeLoad-EDGE-187302"></div>
-        <div id="Stage_Pasted5" class="edgeLoad-EDGE-187302"></div>
-        <div id="Stage_Pasted6" class="edgeLoad-EDGE-187302"></div>
-        <div id="Stage_Pasted7" class="edgeLoad-EDGE-187302"></div>
-        <div id="Stage_Pasted8" class="edgeLoad-EDGE-187302"></div>
-        <div id="Stage_Pasted9" class="edgeLoad-EDGE-187302"></div>
-        <div id="Stage_Pasted10" class="edgeLoad-EDGE-187302"></div>
-        <div id="Stage_Pasted13" class="edgeLoad-EDGE-187302"></div>
-    </div>
+<div id="Stage" class="EDGE-187302">
+
+	<div id="Stage_root2" class="edgeLoad-EDGE-187302"></div>
+	<div id="Stage__1" class="edgeLoad-EDGE-187302"></div>
+	<div id="Stage_Pasted2" class="edgeLoad-EDGE-187302"></div>
+	<div id="Stage_Pasted112" class="edgeLoad-EDGE-187302"></div>
+	<div id="Stage_Pasted4" class="edgeLoad-EDGE-187302"></div>
+	<div id="Stage_Pasted5" class="edgeLoad-EDGE-187302"></div>
+	<div id="Stage_Pasted6" class="edgeLoad-EDGE-187302"></div>
+	<div id="Stage_Pasted7" class="edgeLoad-EDGE-187302"></div>
+	<div id="Stage_Pasted8" class="edgeLoad-EDGE-187302"></div>
+	<div id="Stage_Pasted9" class="edgeLoad-EDGE-187302"></div>
+	<div id="Stage_Pasted10" class="edgeLoad-EDGE-187302"></div>
+	<div id="Stage_Pasted13" class="edgeLoad-EDGE-187302"></div>
+</div>
